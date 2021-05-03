8f0877d855f89504cf8eece5a0ff619a4f840718
cpyberry <cpyberry222@gmail.com>
2021-05-03 15:21:30 +0900

    feat: add a function throw_winsock_error
    Throw an appropriate exception if an error occurs in winsock.

--- include/socketpp/winsock_error.hpp
+++ include/socketpp/winsock_error.hpp
@@ -2,4 +2,21 @@
 #define _WINSOCK_ERROR_HPP
 
 
+#include <sstream>
+#include <stdexcept>
+
+#include <WinSock2.h>
+
+
+namespace winsock_error {
+[[noreturn]] void throw_winsock_error()
+{
+	// Throw an appropriate exception if an error occurs in winsock.
+	std::ostringstream message;
+	message << "error_code: " << ::WSAGetLastError();
+	throw std::runtime_error(message.str());
+}
+}  // namespace winsock_error
+
+
 #endif  // _WINSOCK_ERROR_HPP
