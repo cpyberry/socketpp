5d0711bcd2f287ed115c71b78000c2191e22e677
cpyberry <cpyberry222@gmail.com>
2021-05-03 15:40:05 +0900

    feat: add a function _initial_wsadata

--- include/socketpp/socketpp.hpp
+++ include/socketpp/socketpp.hpp
@@ -2,4 +2,26 @@
 #define _SOCKETPP_HPP
 
 
+#include <winsock2.h>
+
+#include "socketpp/winsock_error.hpp"
+
+
+namespace socketpp {
+class Socket
+{
+private:
+	WSADATA _initial_wsadata(const int& major_version = 2, const int& minor_version = 0) const
+	{
+		WSADATA sock;
+		int result = ::WSAStartup(MAKEWORD(major_version, minor_version), &sock);
+		if (result) {
+			winsock_error::throw_winsock_error();
+		}
+		return sock;
+	}
+};
+}  // namespace socketpp
+
+
 #endif  // _SOCKETPP_HPP
