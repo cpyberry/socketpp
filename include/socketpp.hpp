418f6630b47eed838ce0996ef5d0932279baac2a
cpyberry <cpyberry222@gmail.com>
2021-05-05 23:16:25 +0900

    feat: add include guard and NOTICE

---
+++ include/socketpp.hpp
@@ -0,0 +1,15 @@
+/*
+Copyright 2021 cpyberry
+https://github.com/cpyberry/socketpp
+
+cpyberry
+email: cpyberry222@gmail.com
+github: https://github.com/cpyberry
+*/
+
+
+#ifndef _SOCKETPP_INCLUDE_HPP
+#define _SOCKETPP_INCLUDE_HPP
+
+
+#endif  // _SOCKETPP_INCLUDE_HPP
