401c945ab31069a59dbcdf14a713a035b64c29dd
cpyberry <cpyberry222@gmail.com>
2021-05-07 18:53:56 +0900

    feat: add a function bind
    The function have an instance of Address as an argument.

--- include/socketpp/socketpp.hpp
+++ include/socketpp/socketpp.hpp
@@ -145,6 +145,11 @@ public:
 		}
 	}
 
+	void bind(const Address& bind_address)
+	{
+		this->bind(bind_address.ip_address(), bind_address.port());
+	}
+
 	void connect(const std::string_view& ip_address, const std::uint16_t& port)
 	{
 		this->_set_address(ip_address, port);
